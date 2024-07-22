35c53e89a5ca32bfc20ac1958b25ad35023064f0
codearmadillo <jiri.kralovec17@gmail.com>
2024-07-22 09:52:27 +0200

    shader code clean-up

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { Shader } from './shaders';
 
 /**
  * Checklist:
- * - Init WebGL in canvas
- * - Get context
  * - Build cube
  *  - VAO = VBO + IBO
  * - Render cube
@@ -19,8 +17,10 @@ if (webgl === null) {
   throw new Error('Failed to obtain WebGl2 context');
 }
 
+const shader = new Shader(webgl);
+
+webgl.useProgram(shader.program);
+
 webgl.clearColor(0.0, 0.0, 0.0, 1.0);
 webgl.clear(webgl.COLOR_BUFFER_BIT);
 
-const shader = new Shader(webgl);
-shader.bind();
--- src/shaders.ts
+++ src/shaders.ts
@@ -20,49 +20,49 @@ export const fragmentShaderSource = `
 `;
 
 export class Shader {
-  private readonly program: WebGLProgram | null;
-  private readonly webgl: WebGL2RenderingContext;
+  private readonly $program: WebGLProgram | null;
+  private readonly $webgl: WebGL2RenderingContext;
+
+  public get program() {
+    return this.$program;
+  }
 
   constructor(webgl: WebGL2RenderingContext) {
-    this.webgl = webgl;
-    this.program = webgl.createProgram();
-    if (!this.program) {
+    this.$webgl = webgl;
+    this.$program = webgl.createProgram();
+    if (!this.$program) {
       throw new Error('Failed to create shader program');
     }
 
-    const fragmentShader = this.createShader(webgl.FRAGMENT_SHADER, webgl);
-    const vertexShader = this.createShader(webgl.VERTEX_SHADER, webgl);
+    const fragmentShader = this.createShader(webgl.FRAGMENT_SHADER);
+    const vertexShader = this.createShader(webgl.VERTEX_SHADER);
     
-    webgl.attachShader(this.program, fragmentShader);
-    webgl.attachShader(this.program, vertexShader);
+    webgl.attachShader(this.$program, fragmentShader);
+    webgl.attachShader(this.$program, vertexShader);
 
-    webgl.linkProgram(this.program);
+    webgl.linkProgram(this.$program);
 
-    if (!webgl.getProgramParameter(this.program, webgl.LINK_STATUS)) {
-      const log = webgl.getProgramInfoLog(this.program);
+    if (!webgl.getProgramParameter(this.$program, webgl.LINK_STATUS)) {
+      const log = webgl.getProgramInfoLog(this.$program);
       throw new Error(`Failed to link shader program: ${log}`);
     }
 
-    webgl.detachShader(this.program, fragmentShader);
-    webgl.detachShader(this.program, vertexShader);
+    webgl.detachShader(this.$program, fragmentShader);
+    webgl.detachShader(this.$program, vertexShader);
 
     webgl.deleteShader(fragmentShader);
     webgl.deleteBuffer(vertexShader);
   }
 
-  bind() {
-    this.webgl.useProgram(this.program);
-  }
-
   private createShader(type: number): WebGLShader {
-    const source = type === this.webgl.FRAGMENT_SHADER ? fragmentShaderSource : vertexShaderSource;
-    const shader = this.webgl.createShader(type);
+    const source = type === this.$webgl.FRAGMENT_SHADER ? fragmentShaderSource : vertexShaderSource;
+    const shader = this.$webgl.createShader(type);
     if (!shader) {
-      throw new Error(`Failed to create ${type === this.webgl.FRAGMENT_SHADER ? 'fragment' : 'vertex'} shader`);
+      throw new Error(`Failed to create ${type === this.$webgl.FRAGMENT_SHADER ? 'fragment' : 'vertex'} shader`);
     }
 
-    this.webgl.shaderSource(shader, source);
-    this.webgl.compileShader(shader);
+    this.$webgl.shaderSource(shader, source);
+    this.$webgl.compileShader(shader);
 
     return shader;
   }
