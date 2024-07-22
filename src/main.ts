50ef60a26d81039adb4dec74b5ffa34c3099a95d
codearmadillo <jiri.kralovec17@gmail.com>
2024-07-22 10:24:04 +0200

    converted to vao

--- src/main.ts
+++ src/main.ts
@@ -32,14 +32,30 @@ const points = [
 const vbo = webgl.createBuffer();
 webgl.bindBuffer(webgl.ARRAY_BUFFER, vbo);
 webgl.bufferData(webgl.ARRAY_BUFFER, new Float32Array(points), webgl.STATIC_DRAW);
+webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
 
+// Indices
+const indices = [
+  0, 1, 2
+]
+const ibo = webgl.createBuffer();
+webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, ibo);
+webgl.bufferData(webgl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), webgl.STATIC_DRAW);
+webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, null);
+
+// VAO
+const vao = webgl.createVertexArray();
+webgl.bindVertexArray(vao);
+
+webgl.bindBuffer(webgl.ARRAY_BUFFER, vbo);
 webgl.vertexAttribPointer(0, 3, webgl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
 webgl.enableVertexAttribArray(0);
-
 webgl.vertexAttribPointer(1, 3, webgl.FLOAT, false, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
 webgl.enableVertexAttribArray(1);
 
-webgl.bindBuffer(webgl.ARRAY_BUFFER, null);
+webgl.bindBuffer(webgl.ELEMENT_ARRAY_BUFFER, ibo);
+
+webgl.bindVertexArray(null);
 
 // Configure webgl
 webgl.enable(webgl.BLEND);
@@ -51,5 +67,5 @@ webgl.useProgram(shader.program);
 webgl.clearColor(0.0, 0.0, 0.0, 1.0);
 webgl.clear(webgl.COLOR_BUFFER_BIT);
 
-webgl.bindBuffer(webgl.ARRAY_BUFFER, vbo);
-webgl.drawArrays(webgl.TRIANGLES, 0, 3);
+webgl.bindVertexArray(vao);
+webgl.drawElements(webgl.TRIANGLES, indices.length, webgl.UNSIGNED_SHORT, 0);
